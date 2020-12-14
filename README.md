b6efac9147e658cd830ec4ff4606df4ab0899549
Kaito Kusumoto <zihhkumi@icloud.com>
2020-12-14 21:09:34 +0900

    readme追加

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# teamg
+# teamg 筋トレアプリ(仮)
+
+***
+
+## ディレクトリ説明
+
+### images
+
+動画を読み込んだ際に生成されたデータフレームをcsv形式で保存しています。  
+命名方法は {動画の名前}_{x,y座標のどちらか}_df.csv のようになっています。  
+ex) squat1_x_df.csv　→　squat1 動画の x 座標のデータフレームをcsv化したもの  
+
+### Scripts
+
+ここに基本的にpyファイルやposenet-pythonが入っていたりします。
+
+- *.mp4: 
+    - 動画を入れています。今回使ったデモ動画は squat1.mp4,squat2.mp4 です。
+- modules.py: 
+    - データ分析に用いる関数をまとめています。  現在 「読み込み→標準化→(グラフ描画)→安定度、深さを出す」までを実行しています。
+- conduct.py:
+    - modules.pyにある関数を実行します。
+- fig_graph.ipynb:
+    - 残骸です。
+- posenet-python-master:
+    - webcam_demo.pyのみ動かせれば問題無いです。使用方法は、「python3 posenet-python-master/webcam_demo.py --file squat2.mp4」のように打ち込んでください(Scripts)  詳しくは[github](https://github.com/rwightman/posenet-python)に載っています。
+
+### 問題点
+- webcam_demo.pyでsquat2の読み込みが途中で終わってしまう。
+- 動画の秒数が取得できれば負荷率が出せたり幅が広がる→ffmpegというモジュールを使えばなんとかなる？
--- Scripts/conduct.py
+++ Scripts/conduct.py
@@ -7,28 +7,36 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 
-"""読み込みと標準化"""
+"""
+読み込み→標準化→(グラフ描画)→安定度、深さを出す
 
-#読み込み
-squat1_x_df = pd.read_csv(f'../images/squat1_x_df.csv',index_col=0)
-squat1_y_df = pd.read_csv(f'../images/squat1_y_df.csv',index_col=0)
-squat2_x_df = pd.read_csv(f'../images/squat2_x_df.csv',index_col=0)
-squat2_y_df = pd.read_csv(f'../images/squat2_y_df.csv',index_col=0)
+【問題点】
+webcam_demo.pyでsquat2の読み込みが途中で終わってしまう。
+動画の秒数が取得できれば負荷率が出せたり幅が広がる→ffmpegというモジュールを使えばなんとかなる？
 
+"""
 
-#標準化
-squat1_x_df = modules.replace_second(modules.standarizatioin_df(squat1_x_df))
-squat1_y_df = modules.replace_second(modules.standarizatioin_df(squat1_y_df))
-squat2_x_df = modules.replace_second(modules.standarizatioin_df(squat2_x_df))
-squat2_y_df = modules.replace_second(modules.standarizatioin_df(squat2_y_df))
-
-
-#該当のdfをグラフ化
-modules.draw_graph(squat1_y_df)
-modules.draw_graph(squat2_y_df)
+if __name__ == "__main__":
+    #読み込み
+    squat1_x_df = pd.read_csv(f'../images/squat1_x_df.csv',index_col=0)
+    squat1_y_df = pd.read_csv(f'../images/squat1_y_df.csv',index_col=0)
+    squat2_x_df = pd.read_csv(f'../images/squat2_x_df.csv',index_col=0)
+    squat2_y_df = pd.read_csv(f'../images/squat2_y_df.csv',index_col=0)
 
 
-#安定度、深さを出す関数
-modules.horizontal_stability(squat1_x_df)
-modules.horizontal_stability(squat2_x_df)
-modules.down_depth(squat1_y_df)
+    #標準化
+    squat1_x_df = modules.replace_second(modules.standarizatioin_df(squat1_x_df))
+    squat1_y_df = modules.replace_second(modules.standarizatioin_df(squat1_y_df))
+    squat2_x_df = modules.replace_second(modules.standarizatioin_df(squat2_x_df))
+    squat2_y_df = modules.replace_second(modules.standarizatioin_df(squat2_y_df))
+
+
+    #該当のdfをグラフ化
+    modules.draw_graph(squat1_y_df)
+    modules.draw_graph(squat2_y_df)
+
+
+    #安定度、深さを出す関数
+    modules.horizontal_stability(squat1_x_df)
+    modules.horizontal_stability(squat2_x_df)
+    modules.down_depth(squat1_y_df)
